4582c8e057dae09b0e8548db7587347bd79627bb
Laivy <dnlfg.contato@gmail.com>
2024-05-28 23:19:22 -0300

    added messages

---
+++ src/main/java/codes/laivy/jhttp/message/Message.java
@@ -0,0 +1,21 @@
+package codes.laivy.jhttp.message;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Closeable;
+import java.nio.charset.Charset;
+
+public interface Message extends Closeable {
+
+    @NotNull Charset getCharset();
+
+    byte[] getContent();
+
+    long size();
+
+    static @NotNull StringMessage create(@NotNull String message, @NotNull Charset charset) {
+        byte[] bytes = message.getBytes();
+        return new StringMessage(bytes, charset);
+    }
+
+}
---
+++ src/main/java/codes/laivy/jhttp/message/StringMessage.java
@@ -0,0 +1,40 @@
+package codes.laivy.jhttp.message;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public final class StringMessage implements Message {
+
+    private final @NotNull Charset charset;
+    private final byte @NotNull [] bytes;
+
+    public StringMessage(@NotNull String message, @NotNull Charset charset) {
+        this(message.getBytes(charset), charset);
+    }
+    public StringMessage(byte @NotNull [] bytes, @NotNull Charset charset) {
+        this.bytes = bytes;
+        this.charset = charset;
+    }
+
+    @Override
+    public @NotNull Charset getCharset() {
+        return charset;
+    }
+
+    @Override
+    public byte[] getContent() {
+        return bytes;
+    }
+
+    @Override
+    public long size() {
+        return bytes.length;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
