11a5b9f885664801c0c615732fbed6e19fdfd83d
Laivy <dnlfg.contato@gmail.com>
2024-05-28 23:18:58 -0300

    improved code

---
+++ src/main/java/codes/laivy/jhttp/utilities/Method.java
@@ -0,0 +1,15 @@
+package codes.laivy.jhttp.utilities;
+
+public enum Method {
+
+    POST,
+    DELETE,
+    GET,
+    HEAD,
+    PUT,
+    TRACE,
+    CONNECT,
+    OPTIONS,
+    PATCH
+
+}
---
+++ src/main/java/codes/laivy/jhttp/utilities/NameValuePair.java
@@ -0,0 +1,36 @@
+package codes.laivy.jhttp.utilities;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
+
+import java.util.Objects;
+
+public interface NameValuePair {
+
+    @UnknownNullability String getName();
+    @UnknownNullability String getValue();
+
+    static @NotNull NameValuePair create(final @Nullable String name, final @Nullable String value) {
+        return new NameValuePair() {
+            @Override
+            public @UnknownNullability String getName() {
+                return name;
+            }
+            @Override
+            public @UnknownNullability String getValue() {
+                return value;
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return obj instanceof NameValuePair && ((NameValuePair) obj).getName().equals(name);
+            }
+            @Override
+            public int hashCode() {
+                return Objects.hash(getName());
+            }
+        };
+    }
+
+}
