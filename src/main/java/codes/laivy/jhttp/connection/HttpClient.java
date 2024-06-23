ffac5bd068b143c83b4954e904d17db7834f7d94
Daniel Richard <dnlfg.contato@gmail.com>
2024-06-23 20:06:12 -0300

    added javadocs to http client and secure http client

--- src/main/java/codes/laivy/jhttp/connection/HttpClient.java
+++ src/main/java/codes/laivy/jhttp/connection/HttpClient.java
@@ -2,33 +2,72 @@ package codes.laivy.jhttp.connection;
 
 import codes.laivy.jhttp.element.request.HttpRequest;
 import codes.laivy.jhttp.element.response.HttpResponse;
-import org.jetbrains.annotations.Blocking;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.io.Closeable;
-import java.io.Flushable;
+import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * A client interface for making HTTP requests.
+ *
+ * <p>This interface provides methods to read and write HTTP requests and responses,
+ * manage the connection status, and obtain the underlying socket.</p>
+ *
+ * @see Closeable
+ * @see Socket
+ *
+ * @author Daniel Richard (Laivy)
+ * @since 1.0-SNAPSHOT
+ */
+@ApiStatus.Experimental
 public interface HttpClient extends Closeable {
 
-    @NotNull Socket getSocket();
+    /**
+     * Retrieves the underlying socket of the HTTP client.
+     *
+     * @return The socket associated with this HTTP client.
+     * @throws IOException If an I/O error occurs while retrieving the socket.
+     * @since 1.0
+     */
+    @NotNull
+    Socket getSocket() throws IOException;
 
-    @Blocking
-    @NotNull HttpRequest read();
-    void write(@NotNull HttpResponse response);
+    /**
+     * Reads an HTTP request from the client.
+     *
+     * @return The HTTP request read from the client.
+     * @throws IOException If an I/O error occurs while reading the request.
+     * @since 1.0
+     */
+    @NotNull
+    HttpRequest read() throws IOException;
 
+    /**
+     * Writes an HTTP response to the client.
+     *
+     * @param response The HTTP response to be written.
+     * @throws IOException If an I/O error occurs while writing the response.
+     * @since 1.0
+     */
+    void write(@NotNull HttpResponse response) throws IOException;
+
+    /**
+     * Checks whether the HTTP client is open or not.
+     *
+     * @return {@code true} if the client is open, {@code false} otherwise.
+     * @since 1.0
+     */
     boolean isOpen();
-    
-    @NotNull Metrics getAnalytic();
 
-    // Classes
-
-    interface Metrics extends Flushable {
-        long getRequestCount();
-        long getResponseCount();
-
-        long getSentBytesCount();
-        long getReceivedBytesCount();
-    }
-
+    /**
+     * Retrieves a client instance associated with this HTTP client.
+     *
+     * @return A client instance associated with this HTTP client.
+     * @since 1.0
+     */
+    @UnknownNullability
+    HttpClient getClient();
 }
--- src/main/java/codes/laivy/jhttp/connection/SecureHttpClient.java
+++ src/main/java/codes/laivy/jhttp/connection/SecureHttpClient.java
@@ -1,9 +1,34 @@
 package codes.laivy.jhttp.connection;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
 import javax.net.ssl.SSLSession;
 
+/**
+ * A secure extension of the {@link HttpClient} interface that provides support for SSL/TLS.
+ *
+ * <p>This interface extends the base {@link HttpClient} interface to add methods for obtaining
+ * an SSL session.</p>
+ *
+ * @see HttpClient
+ * @see SSLSession
+ *
+ * @author Daniel Richard (Laivy)
+ * @since 1.0-SNAPSHOT
+ */
+@ApiStatus.Experimental
 public interface SecureHttpClient extends HttpClient {
-    @Nullable SSLSession getSession();
+
+    /**
+     * Retrieves the SSL session associated with the secure HTTP client.
+     *
+     * <p>This method returns {@code null} if no SSL session is established.</p>
+     *
+     * @return The SSL session associated with the secure HTTP client, or {@code null} if no session is established.
+     * @since 1.0
+     */
+    @Nullable
+    SSLSession getSession();
+    
 }
