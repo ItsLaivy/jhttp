6cfdbd18aaf7983b0484c6966f82955186c239c5
Daniel Richard <dnlfg.contato@gmail.com>
2024-07-09 20:24:09 -0300

    improved and fixed email parser issues

--- src/main/java/codes/laivy/jhttp/url/email/Email.java
+++ src/main/java/codes/laivy/jhttp/url/email/Email.java
@@ -1,14 +1,12 @@
 package codes.laivy.jhttp.url.email;
 
-import codes.laivy.jhttp.url.Host.Name;
-import codes.laivy.jhttp.url.domain.Domain;
+import codes.laivy.jhttp.url.domain.SLD;
 import codes.laivy.jhttp.url.domain.Subdomain;
+import codes.laivy.jhttp.url.domain.TLD;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Email implements CharSequence {
@@ -17,81 +15,123 @@ public class Email implements CharSequence {
 
     private static final @NotNull Pattern EMAIL_REGEX = Pattern.compile("^(?<username>[a-zA-Z0-9._%+-]{1,64})@(?<domain>[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$");
 
-    public static @NotNull Email parse(@NotNull String string) throws IllegalArgumentException {
+    public static boolean validate(@NotNull String string) {
         string = string.replace("(dot)", ".").replace("(at)", "@");
-        @NotNull Matcher matcher = EMAIL_REGEX.matcher(string);
 
-        if (matcher.matches()) {
-            @NotNull String username = matcher.group("username");
-            @NotNull Domain<Name> domain;
+        if (string.length() > 2048) {
+            return false;
+        } else if (string.split("@").length != 2) {
+            return false;
+        } else {
+            @NotNull String[] parts = string.split("@");
 
-            try {
-                //noinspection unchecked
-                domain = (Domain<Name>) Domain.parse(matcher.group("domain"));
-            } catch (@NotNull ClassCastException | @NotNull IllegalArgumentException e) {
-                throw new IllegalArgumentException("invalid host name '" + matcher.group("domain") + "'", e);
+            if (!parts[0].matches("[a-zA-Z0-9._%+-]{1,64}")) {
+                return false;
             }
 
-            return create(username, domain);
+            parts = parts[1].split("\\.");
+
+            @NotNull String[] subdomains = parts.length > 2 ? Arrays.stream(Arrays.copyOfRange(parts, 0, parts.length - 2)).toArray(String[]::new) : new String[0];
+            @NotNull String sld = parts[parts.length - 2];
+            @NotNull String tld = parts[parts.length - 1];
+
+            return Arrays.stream(subdomains).allMatch(Subdomain::validate) && SLD.validate(sld) && TLD.validate(tld);
+        }
+    }
+    public static @NotNull Email parse(@NotNull String string) throws IllegalArgumentException {
+        if (string.length() > 2048) {
+            throw new IllegalArgumentException("email too long!");
+        }
+
+        string = string.replace("(dot)", ".").replace("(at)", "@");
+
+        if (validate(string)) {
+            @NotNull String[] parts = string.split("@");
+
+            @NotNull String username = parts[0];
+            parts = parts[1].split("\\.");
+
+            @NotNull Subdomain[] subdomains = parts.length > 2 ? Arrays.stream(Arrays.copyOfRange(parts, 0, parts.length - 2)).map(Subdomain::create).toArray(Subdomain[]::new) : new Subdomain[0];
+            @NotNull SLD sld = SLD.parse(parts[parts.length - 2]);
+            @NotNull TLD tld = TLD.parse(parts[parts.length - 1]);
+
+            return create(username, subdomains, sld, tld);
         } else {
-            throw new IllegalArgumentException("cannot parse '" + string + "' as a valid e-mail");
+            throw new IllegalArgumentException("cannot parse '" + string + "' as a valid e-mail address");
         }
     }
 
     public static @NotNull Email create(
             final @NotNull String username,
-            final @NotNull Domain<Name> domain
+
+            final @NotNull Subdomain @NotNull [] subdomains,
+            final @NotNull SLD sld,
+            final @NotNull TLD tld
     ) throws IllegalArgumentException {
         @NotNull String string = username.replace("(dot)", ".").replace("(at)", "@");
-        return new Email(username, domain);
+        return new Email(username, subdomains, sld, tld);
     }
 
     // Object
 
     private final @NotNull String username;
-    private final @NotNull Domain<Name> domain;
 
-    protected Email(@NotNull String username, @NotNull Domain<Name> domain) {
+    private final @NotNull Subdomain @NotNull [] subdomains;
+    private final @NotNull SLD sld;
+    private final @NotNull TLD tld;
+
+    protected Email(@NotNull String username, @NotNull Subdomain @NotNull [] subdomains, @NotNull SLD sld, @NotNull TLD tld) {
         if (!username.matches("[a-zA-Z0-9._%+-]{1,64}")) {
             throw new IllegalArgumentException("invalid email username format '" + username + "'");
         }
 
         this.username = username;
-        this.domain = domain;
+        this.subdomains = subdomains;
+        this.sld = sld;
+        this.tld = tld;
     }
 
     // Getters
 
-    public @NotNull String getUsername() {
+    public final @NotNull String getUsername() {
         return username;
     }
-    public @NotNull Domain<Name> getDomain() {
-        return domain;
+
+    public @NotNull Subdomain @NotNull [] getSubdomains() {
+        return subdomains;
+    }
+
+    public @NotNull SLD getSLD() {
+        return sld;
+    }
+    public @NotNull TLD getTLD() {
+        return tld;
     }
 
     // Implementations
 
     @Override
-    public final boolean equals(@Nullable Object object) {
+    public final boolean equals(Object object) {
         if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        @NotNull Email email = (Email) object;
-        return Objects.equals(getUsername(), email.getUsername()) && Objects.equals(getDomain().getName(), email.getDomain().getName()) && Arrays.equals(getDomain().getSubdomains(), email.domain.getSubdomains());
+        if (!(object instanceof Email)) return false;
+        Email email = (Email) object;
+        return Objects.equals(getUsername(), email.getUsername()) && Objects.deepEquals(getSubdomains(), email.getSubdomains()) && Objects.equals(sld, email.sld) && Objects.equals(tld, email.tld);
     }
     @Override
     public final int hashCode() {
-        return Objects.hash(getUsername(), getDomain().getName(), Arrays.hashCode(getDomain().getSubdomains()));
+        return Objects.hash(getUsername(), Arrays.hashCode(getSubdomains()), sld, tld);
     }
 
     @Override
     public final @NotNull String toString() {
-        @NotNull StringBuilder builder = new StringBuilder(getUsername()).append("@");
+        @NotNull StringBuilder builder = new StringBuilder(getUsername());
+        builder.append("@");
 
-        for (@NotNull Subdomain subdomain : getDomain().getSubdomains()) {
-            builder.append(subdomain.getValue()).append(".");
+        for (@NotNull Subdomain subdomain : getSubdomains()) {
+            builder.append(subdomain).append(".");
         }
+        builder.append(getSLD()).append(".").append(getTLD());
 
-        builder.append(getDomain().getName());
         return builder.toString();
     }
 
