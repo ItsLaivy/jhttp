0b9167c6d670b601d9825606dca74bb4b0238cbb
Daniel Richard <dnlfg.contato@gmail.com>
2024-07-11 08:16:46 -0300

    improved clients and added metrics

--- src/main/java/codes/laivy/jhttp/client/HttpClient.java
+++ src/main/java/codes/laivy/jhttp/client/HttpClient.java
@@ -1,55 +1,85 @@
 package codes.laivy.jhttp.client;
 
+import codes.laivy.jhttp.element.HttpMetrics;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.channels.ByteChannel;
+import java.nio.channels.ClosedChannelException;
+import java.time.Duration;
 
 /**
- * A client interface for making HTTP requests.
- *
- * <p>This interface provides methods to manage the connection status, and obtain the underlying socket.</p>
- *
- * @see Closeable
- * @see Socket
+ * Interface representing an HTTP client.
+ * This interface provides methods to retrieve socket information, address details,
+ * HTTP metrics, and proxy usage.
+ * <p>
+ * Additionally, it includes methods for checking
+ * the open status of the client and closing the connection.
  *
  * @author Daniel Richard (Laivy)
- * @since 1.0-SNAPSHOT
+ * @version 1.0-SNAPSHOT
  */
 public interface HttpClient extends Closeable {
 
     /**
-     * Checks if this client is secure
+     * Retrieves the underlying socket used by the HTTP client.
      *
-     * @return True if the client is secure; false otherwise
-     * @since 1.0
-     */
-    default boolean isSecure() {
-        return this instanceof SecureHttpClient;
-    }
-
-    /**
-     * Retrieves the underlying socket of the HTTP client.
-     *
-     * @return The socket associated with this HTTP client.
-     * @since 1.0
+     * @return the {@link Socket} used by the HTTP client; never null.
      */
     @NotNull Socket getSocket();
 
     /**
-     * Checks whether the HTTP client is open or not.
+     * Retrieves the address of the HTTP client.
+     *
+     * @return the {@link SocketAddress} of the HTTP client; never null.
+     */
+    @NotNull SocketAddress getAddress();
+
+    /**
+     * Retrieves the metrics associated with the HTTP client's operations.
+     *
+     * @return the {@link HttpMetrics} related to the HTTP client's activities;
+     *         never null.
+     */
+    @NotNull HttpMetrics getMetrics();
+
+    /**
+     * Checks if the HTTP client is using a proxy server.
+     *
+     * @return {@code true} if the client is using a proxy, {@code false} otherwise.
+     * @throws NullPointerException caused if there are no previous headers to see if it is a proxy
+     */
+    boolean hasProxy() throws NullPointerException;
+
+    /**
+     * Checks if the HTTP client is currently open.
      *
      * @return {@code true} if the client is open, {@code false} otherwise.
-     * @since 1.0
      */
     boolean isOpen();
 
     /**
-     * Closes the http client connection
+     * Closes the HTTP client immediately.
      *
-     * @since 1.0
+     * @throws ClosedChannelException if the channel is already closed.
+     * @throws IOException if an I/O error occurs while closing the client.
      */
-    void close() throws IOException;
+    default void close() throws ClosedChannelException, IOException {
+        close(Duration.ZERO);
+    }
+
+    /**
+     * Closes the HTTP client with a specified timeout duration.
+     * It will await for the current tasks to finish before shutdown the connection
+     *
+     * @param timeout the duration to wait before closing the client;
+     *                must not be null.
+     * @throws ClosedChannelException if the channel is already closed.
+     * @throws IOException if an I/O error occurs while closing the client.
+     */
+    void close(@NotNull Duration timeout) throws ClosedChannelException, IOException;
 
 }
